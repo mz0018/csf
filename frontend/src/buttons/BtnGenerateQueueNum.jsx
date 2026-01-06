66b337a021a5d1a6ebcc7ab7829a2019949740be
mz0018 <martinezhanzmenzi@gmail.com>
2026-01-06 16:56:34 +0800

    Add a Loader on Button Generate Queue using lucide-react

--- frontend/src/buttons/BtnGenerateQueueNum.jsx
+++ frontend/src/buttons/BtnGenerateQueueNum.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import api from "../services/api";
+import { TicketPlus, Loader } from "lucide-react";
 
 const BtnGenerateQueueNum = () => {
     const [queue, setQueue] = useState([]);
@@ -8,14 +9,16 @@ const BtnGenerateQueueNum = () => {
     const { user } = useAuth();
 
     const handleClick = async () => {
-
         if (!user.officeId) {
-            console.error("User has no officed assigned.");
+            console.error("User has no office assigned.");
             return;
         }
 
         try {
             setLoading(true);
+
+            // await new Promise((resolve) => setTimeout(resolve, 10000));
+
             const response = await api.post(`client/queue/generate/${user.officeId}`);
             
             if (response.data?.success === true) {
@@ -24,21 +27,34 @@ const BtnGenerateQueueNum = () => {
 
         } catch (err) {
             console.error('Something went wrong: ', err);
-            debugger
         } finally {
             setLoading(false);
         }
-    }
+    };
 
     useEffect(() => {
-        console.table(queue)
-    }, [queue])
+        console.table(queue);
+    }, [queue]);
 
     return (
-        <button className="bg-[var(--button-color)] text-white p-4 rounded-sm cursor-pointer" disabled={loading} onClick={() => handleClick()}>
-            {loading ? 'Loading...' : 'Generate Queue Number'}
+        <button
+            onClick={handleClick}
+            disabled={loading}
+            className="text-sm flex items-center gap-2 bg-[var(--button-color)] hover:bg-[var(--btn-hover-color)] text-white px-6 py-4 rounded-sm cursor-pointer transition tracking-wide justify-center"
+        >
+            {loading ? (
+                <>
+                    <Loader size={20} className="animate-spin" />
+                    <span>Generating...</span>
+                </>
+            ) : (
+                <>
+                    <TicketPlus size={25} className="rotate-45" />
+                    <span>Generate Queue Number</span>
+                </>
+            )}
         </button>
-    )
-}
+    );
+};
 
 export default BtnGenerateQueueNum;
