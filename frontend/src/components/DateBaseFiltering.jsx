24b7960893cf26c15d82a78184b7924584ce3286
mz0018 <martinezhanzmenzi@gmail.com>
2026-01-19 13:30:24 +0800

    responsive layout on dailyStats

--- frontend/src/components/DateBaseFiltering.jsx
+++ frontend/src/components/DateBaseFiltering.jsx
@@ -10,19 +10,30 @@ const DateBaseFiltering = ({ officeId }) => {
     const { waiting = [], completed = [], expired = [] } = list;
 
     return (
-        <section>
+        <section className="space-y-4">
             <h2 className="text-2xl font-bold tracking-wide">Queue Counts for Today</h2>
 
-            <div className="grid grid-cols-2 gap-4">
-                <div className="bg-[var(--table-color)]">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="bg-[var(--table-color)] p-5">
                     <p>Total: {waiting.length + completed.length + expired.length}</p>
                 </div>
 
-                <div className="bg-[var(--table-color)] grid grid-cols-3 gap-2">
-                    <div>Waiting: {waiting.length}</div>
-                    <div>Completed: {completed.length}</div>
-                    <div>Expired: {expired.length}</div>    
+                <div className="bg-[var(--table-color)] p-5">
+                    <p className="font-semibold mb-2">Title</p>
+
+                    <div className="grid grid-cols-3 text-sm font-medium text-center mb-1 space-x-3">
+                        <span>Total waiting queue</span>
+                        <span>Total expired queue</span>
+                        <span>Completed</span>
+                    </div>
+
+                    <div className="grid grid-cols-3 text-start text-3xl">
+                        <span>{waiting.length}</span>
+                        <span>{expired.length}</span>
+                        <span>{completed.length}</span>
+                    </div>
                 </div>
+
             </div>
 
         </section>
